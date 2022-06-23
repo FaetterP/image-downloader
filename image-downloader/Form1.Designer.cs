5b0d2faecfedcbb6960967164e7c637cf66838a6
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-06-24 00:17:45 +0300

    Добавлен интерфейс

--- image-downloader/Form1.Designer.cs
+++ image-downloader/Form1.Designer.cs
@@ -29,13 +29,94 @@ namespace image_downloader
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.selectedFolderText = new System.Windows.Forms.Label();
+            this.selectSourceDropdown = new System.Windows.Forms.ComboBox();
+            this.sourceLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(12, 55);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(104, 37);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // selectedFolderText
+            // 
+            this.selectedFolderText.AutoSize = true;
+            this.selectedFolderText.Location = new System.Drawing.Point(122, 65);
+            this.selectedFolderText.Name = "selectedFolderText";
+            this.selectedFolderText.Size = new System.Drawing.Size(26, 17);
+            this.selectedFolderText.TabIndex = 1;
+            this.selectedFolderText.Text = "D:\\";
+            // 
+            // selectSourceDropdown
+            // 
+            this.selectSourceDropdown.FormattingEnabled = true;
+            this.selectSourceDropdown.Items.AddRange(new object[] {
+            "prntsc",
+            "imgur"});
+            this.selectSourceDropdown.Location = new System.Drawing.Point(71, 12);
+            this.selectSourceDropdown.Name = "selectSourceDropdown";
+            this.selectSourceDropdown.Size = new System.Drawing.Size(121, 24);
+            this.selectSourceDropdown.TabIndex = 2;
+            // 
+            // sourceLabel
+            // 
+            this.sourceLabel.AutoSize = true;
+            this.sourceLabel.Location = new System.Drawing.Point(12, 15);
+            this.sourceLabel.Name = "sourceLabel";
+            this.sourceLabel.Size = new System.Drawing.Size(53, 17);
+            this.sourceLabel.TabIndex = 3;
+            this.sourceLabel.Text = "Source";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(15, 125);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 32);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 96);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(321, 193);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.sourceLabel);
+            this.Controls.Add(this.selectSourceDropdown);
+            this.Controls.Add(this.selectedFolderText);
+            this.Controls.Add(this.selectFolderButton);
+            this.Name = "Form1";
+            this.Text = "image-downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Label selectedFolderText;
+        private System.Windows.Forms.ComboBox selectSourceDropdown;
+        private System.Windows.Forms.Label sourceLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
